5d3e4ad826e2b78187c8b70fa682b893cbdefa6f
vietducITITIU19103 <Ducnguyenviet91313@gmail.com>
2024-09-12 20:27:08 +0700

    update ui login,register form

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -40,30 +40,31 @@
             </ul>
 
             <ul class="navbar-nav ms-3 mb-2 mb-lg-0">
-                @auth
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{route('profile')}}">
-                            Welcome, {{ Auth::user()->name }}
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <form action="{{ route('logout') }}" method="POST">
-                            @csrf
-                            <button type="submit" class="btn btn-link nav-link" style="text-decoration: none;">Logout</button>
-                        </form>
-                    </li>
-                @else
-                    <li class="btn btn-login">
-                        <a class="nav-link" href="{{ route('login') }}">
-                            <i class="bi bi-box-arrow-in-right me-2"></i>Login
-                        </a>
-                    </li>
-                    <li class="btn btn-signup">
-                        <a class="nav-link" href="{{ route('register') }}">
-                            <i class="bi bi-person-plus-fill me-2"></i>Sign up
-                        </a>
-                    </li>
-                @endauth
+    @auth
+        <li class="nav-item">
+            <a class="nav-link" href="{{route('profile')}}">
+                Welcome, {{ Auth::user()->name }}
+            </a>
+        </li>
+        <li class="nav-item">
+            <form action="{{ route('logout') }}" method="POST">
+                @csrf
+                <button type="submit" class="btn btn-link nav-link" style="text-decoration: none;">Logout</button>
+            </form>
+        </li>
+    @else
+        <li class="nav-item">
+            <a class="btn btn-login" href="{{ route('login') }}">
+                <i class="bi bi-box-arrow-in-right me-2"></i>Login
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="btn btn-signup" href="{{ route('register') }}">
+                <i class="bi bi-person-plus-fill me-2"></i>Sign up
+            </a>
+        </li>
+    @endauth
+</ul>
         </div>
     </div>
 </nav>
